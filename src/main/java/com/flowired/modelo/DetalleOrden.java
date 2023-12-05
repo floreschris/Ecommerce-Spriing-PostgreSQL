34a15b746b729ce4c18f9e7fe653c05d7f04e01c
floreschris <floresnuntony@gmail.com>
2023-12-05 14:47:29 -0500

    Atributo solucionado

--- src/main/java/com/flowired/modelo/DetalleOrden.java
+++ src/main/java/com/flowired/modelo/DetalleOrden.java
@@ -1,5 +1,27 @@
 package com.flowired.modelo;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
+@Table(name = "Detalle_Orden")
 public class DetalleOrden {
+
+	private Integer id_Detalle_Orden;
+	private String name;
+	private double cantidad;
+	private double precio;
+	private double total;
+	private double total_one;
 
 }
--- src/main/java/com/flowired/modelo/Orden.java
+++ src/main/java/com/flowired/modelo/Orden.java
@@ -1,5 +1,28 @@
 package com.flowired.modelo;
 
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
+@Table(name = "Orden")
 public class Orden {
+
+	private Integer id_Orden;
+	private String numero;
+	private Date fecha_Creacion;
+	private Date fecha_Recibida;
+	private double total;
 
 }
